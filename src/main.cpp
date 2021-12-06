a2544a0dec23581fa536492fe7196e7668833172
Abin Joseph <itsmeabjs@gmail.com>
2021-12-06 21:49:58 +0530

    timing funtion

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
+unsigned long previousMillis1 = 0;
+unsigned long previousMillis2 = 0;
+unsigned long previousMillis3 = 0;
+int checkMillisInterval(unsigned long interval, unsigned long *previousMillis)
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - *previousMillis >= interval)
+  {
+    *previousMillis = currentMillis;
+    return 1;
+  }
+  return 0;
+}
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  if (checkMillisInterval(2000, &previousMillis1))
+  {
+    Serial.println("                    ");
+    Serial.println("         2s        ");
+    Serial.println("                    ");
+  }
+  if (checkMillisInterval(5000, &previousMillis2))
+  {
+    Serial.println("                    ");
+    Serial.println("         5s         ");
+    Serial.println("                    ");
+  }
+  if (checkMillisInterval(10000, &previousMillis3))
+  {
+    Serial.println("                    ");
+    Serial.println("         10s        ");
+    Serial.println("                    ");
+  }
 }
